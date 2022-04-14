4b615d79ece3690b366fd4dab2e9e278e54a4d53
leo-esaki <leo@arturo.finance>
2022-04-14 12:53:28 -0500

    Implement a resuable component for connecting with metamask.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import ConnectWalletButton from "./components/ConnectWalletButton"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-app'>
+      <h1>Welcome to Arturo Finance!</h1>
+      <div>
+        {ConnectWalletButton()}
+      </div>
     </div>
   );
 }
